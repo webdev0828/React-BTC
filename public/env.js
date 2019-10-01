236aff1422156759b35051384d07ca96b856ca68
Andrei Iunda <andrei0828@outlook.com>
2019-10-01 07:42:08 +0300

    init public

---
+++ public/env.js
@@ -0,0 +1,14 @@
+window.env = {
+  api: {
+    authUrl: 'https://www.masternodex.com/api/v2/barong',
+    tradeUrl: 'https://www.masternodex.com/api/v2/peatio',
+    applogicUrl: 'https://www.masternodex.com/api/v2/applogic',
+    rangerUrl: 'wss://www.masternodex.com/api/v2/ranger',
+  },
+  minutesUntilAutoLogout: '60',
+  withCredentials: false,
+  captcha: {
+    captchaType: 'none',
+    siteKey: 'changeme',
+  },
+}
